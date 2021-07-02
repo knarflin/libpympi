858e795a413abde21a0a7780487f4372f4139209
Frank Lin <knarf.lin@yahoo.com>
2021-07-02 13:12:10 +0800

    clock_gettime of measure time

--- all_reduce.c
+++ all_reduce.c
@@ -2,11 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 // #define COUNT 1000000
 #define COUNT 2408448  // 2408448 // 2 * 2 * 2 * 6 * 224 * 224
 
 void test_mpi_allreduce() {
+  struct timespec tstart = {0, 0}, tend = {0, 0};
+
   const size_t count = COUNT;
   int repeat_time    = 100;
   // long int sendbuf[COUNT] = {0};
@@ -17,11 +20,16 @@ void test_mpi_allreduce() {
   // int status = MPI_Allreduce(sendbuf, recvbuf, (int)count, MPI_UINT64_T,
   //                            MPI_SUM, MPI_COMM_WORLD);
 
+  clock_gettime(CLOCK_MONOTONIC, &tstart);
   int status = 0;
   for (int i = 0; i < repeat_time; i++) {
     status = MPI_Allreduce(MPI_IN_PLACE, recvbuf, (int)count, MPI_UINT64_T,
                            MPI_SUM, MPI_COMM_WORLD);
   }
+  clock_gettime(CLOCK_MONOTONIC, &tend);
+  printf("some_long_computation took about %.5f seconds\n",
+         ((double)tend.tv_sec + 1.0e-9 * tend.tv_nsec) -
+             ((double)tstart.tv_sec + 1.0e-9 * tstart.tv_nsec));
 
   if (status != 0) {
     printf("MPI_Allreduce retval: %d\n", status);
